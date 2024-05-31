8f8ea2f6ae7f1cbd2621f006f7d21e9d32e68e16
cgreen18 <green456@mnemosyne.ecn.purdue.edu>
2024-05-31 17:45:22 -0400

    extra options

--- topology_generation/gurobi/bash/create_lpbt.sh
+++ topology_generation/gurobi/bash/create_lpbt.sh
@@ -11,7 +11,7 @@ esac
 
 
 # 3 day limit
-BASE_FLAGS="--use_run_sol --time_limit 4320 --write_model --heuristic_ratio 0.8 --mip_focus 1"
+BASE_FLAGS="--use_run_sol --time_limit 4320 --write_model --heuristic_ratio 0.8 --mip_focus 1 --concurrent_mip"
 
 # 5 ports for an extra for node to router conn
 CMD="./bin/lpbt -if files/prob_defs/dev_${1}r_5p_${size_str}.dat -of my_lpbt_${1}r_${2}_topo ${BASE_FLAGS}"
--- topology_generation/gurobi/bash/create_netsmith.sh
+++ topology_generation/gurobi/bash/create_netsmith.sh
@@ -11,7 +11,7 @@ esac
 
 
 # 3 day limit
-BASE_FLAGS="--use_run_sol --time_limit 4320 --write_model --heuristic_ratio 0.8 --mip_focus 1"
+BASE_FLAGS="--use_run_sol --time_limit 4320 --write_model --heuristic_ratio 0.8 --mip_focus 1 --concurrent_mip --model_type w_hop --max_diam 20 --uni_links -o avg_hops"
 
 # 5 ports for an extra for node to router conn
 CMD="./bin/auto_top -if files/prob_defs/dev_${1}r_5p_${size_str}.dat -of my_netsmith_${1}r_${2}_topo ${BASE_FLAGS}"
