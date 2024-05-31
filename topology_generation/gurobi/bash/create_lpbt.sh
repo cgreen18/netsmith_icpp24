4111506ee940b7d8db8f85eab0ef212c3810855a
cgreen18 <green456@mnemosyne.ecn.purdue.edu>
2024-05-31 17:49:24 -0400

    added CLAs for objectives

--- topology_generation/gurobi/bash/create_lpbt.sh
+++ topology_generation/gurobi/bash/create_lpbt.sh
@@ -10,11 +10,18 @@ case $2 in
 esac
 
 
+case $3 in
+    ("latency") obj_str="total_hops" ;;
+    ("power") obj_str="power" ;;
+    (*) echo "Unrecognized objective. Options are latency, power";;
+esac
+
+
 # 3 day limit
 BASE_FLAGS="--use_run_sol --time_limit 4320 --write_model --heuristic_ratio 0.8 --mip_focus 1 --concurrent_mip"
 
 # 5 ports for an extra for node to router conn
-CMD="./bin/lpbt -if files/prob_defs/dev_${1}r_5p_${size_str}.dat -of my_lpbt_${1}r_${2}_topo ${BASE_FLAGS}"
+CMD="./bin/lpbt -if files/prob_defs/dev_${1}r_5p_${size_str}.dat -of my_lpbt_${3}_${1}r_${2}_topo -o ${obj_str} ${BASE_FLAGS}"
 
 echo $CMD
 $CMD
--- topology_generation/gurobi/bash/create_netsmith.sh
+++ topology_generation/gurobi/bash/create_netsmith.sh
@@ -10,11 +10,17 @@ case $2 in
 esac
 
 
+case $3 in
+    ("latency") obj_str="avg_hops" ;;
+    ("sparsest_cut") obj_str="sc_bw" ;;
+    (*) echo "Unrecognized objective. Options are latency, sparsest_cut";;
+esac
+
 # 3 day limit
-BASE_FLAGS="--use_run_sol --time_limit 4320 --write_model --heuristic_ratio 0.8 --mip_focus 1 --concurrent_mip --model_type w_hop --max_diam 20 --uni_links -o avg_hops"
+BASE_FLAGS="--use_run_sol --time_limit 4320 --write_model --heuristic_ratio 0.8 --mip_focus 1 --concurrent_mip --model_type w_hop --max_diam 20 --uni_links"
 
 # 5 ports for an extra for node to router conn
-CMD="./bin/auto_top -if files/prob_defs/dev_${1}r_5p_${size_str}.dat -of my_netsmith_${1}r_${2}_topo ${BASE_FLAGS}"
+CMD="./bin/auto_top -if files/prob_defs/dev_${1}r_5p_${size_str}.dat -of my_netsmith_${3}_${3}_${1}r_${2}_topo -o ${obj_str} ${BASE_FLAGS}"
 
 echo $CMD
 $CMD
