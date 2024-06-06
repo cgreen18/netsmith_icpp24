2247d927c7a3301564338492f8224a8b58df89f9
cgreen18 <green456@mnemosyne.ecn.purdue.edu>
2024-06-06 10:52:38 -0400

    results and scripts for fig 6

---
+++ auto_top_gem5/bash/plot_parsec_fig6.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# python3 python_scripts/graph_parsec.py parsec_results/csvs/noci_nndbt_and_mclb_18nov.csv fig6 numcycles
+
+python3 python_scripts/graph_parsec.py parsec_results/csvs/noci_mclb_and_nndbt_29apr24.csv fig6 numcycles
