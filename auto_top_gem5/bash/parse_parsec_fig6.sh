8d728cb4fbf4d17e433fe83b5c9845834d4a9ca4
cgreen18 <green456@mnemosyne.ecn.purdue.edu>
2024-06-16 00:33:55 -0400

    revised

--- auto_top_gem5/bash/parse_parsec_fig6.sh
+++ auto_top_gem5/bash/parse_parsec_fig6.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-# python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_6envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100mwarmup_100msimul/ parsec_results/csvs/noci_mclb_29apr24.csv
+# python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_6envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100mwarmup_100msimul/ parsec_results/csvs/noci_mclb_$mydate.csv
 
-# python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_2envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100mwarmup_100msimul/ parsec_results/csvs/noci_nndbt_29apr24.csv
+# python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_2envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100mwarmup_100msimul/ parsec_results/csvs/noci_nndbt_$mydate.csv
 
-python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_6envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100000000warmup_100000000simul/ parsec_results/csvs/noci_mclb_29apr24.csv
-
-python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_2envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100000000warmup_100000000simul parsec_results/csvs/noci_nndbt_29apr24.csv
+mydate=$(date +'%d%b%y')
 
 
-cat parsec_results/csvs/noci_mclb_29apr24.csv > parsec_results/csvs/noci_mclb_and_nndbt_29apr24.csv
-tail -n +2 parsec_results/csvs/noci_nndbt_29apr24.csv >> parsec_results/csvs/noci_mclb_and_nndbt_29apr24.csv
+
+python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_6envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100000000warmup_100000000simul/ parsec_results/csvs/noci_mclb_$mydate.csv
+
+python3 python_scripts/parse_parsec_results.py parsec_results/noci_32GBxDDR4_2envs_38GHz_2MBl2_allthreads_64lwidth_sameflowpriority_lwbufadj/100000000warmup_100000000simul parsec_results/csvs/noci_nndbt_$mydate.csv
+
+
+outfile="parsec_results/csvs/noci_mclb_and_nndbt_$mydate.csv"
+
+# cat parsec_results/csvs/noci_mclb_$mydate.csv | grep "picky\|mesh\|bench" > $outfile
+# tail -n +2 parsec_results/csvs/noci_nndbt_$mydate.csv | grep "injej" >> $outfile
+
+cat parsec_results/csvs/noci_mclb_$mydate.csv > $outfile
+tail -n +2 parsec_results/csvs/noci_nndbt_$mydate.csv >> $outfile
+
+echo "Wrote to $outfile"
--- auto_top_gem5/bash/plot_parsec_fig6.sh
+++ auto_top_gem5/bash/plot_parsec_fig6.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 
-# python3 python_scripts/graph_parsec.py parsec_results/csvs/noci_nndbt_and_mclb_18nov.csv fig6 numcycles
-
-python3 python_scripts/graph_parsec.py parsec_results/csvs/noci_mclb_and_nndbt_29apr24.csv fig6 numcycles
+python3 python_scripts/graph_parsec.py parsec_results/csvs/paper_results/noci_mclb_and_nndbt_29apr24.csv fig6 numcycles
