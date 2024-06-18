a190f4175903e818dcba5bc5c3eababee5bd0bbc
cgreen18 <green456@mnemosyne.ecn.purdue.edu>
2024-06-17 23:04:40 -0400

    rearrange build

---
+++ install_tools.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+# for setting up working directory tools
+
+N_PROCS=$(nproc)
+
+# install python 3.11 from source
+
+sudo apt update
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
+tar -xf Python-3.11.3.tgz
+cd Python-3.11.3
+./configure --enable-optimizations
+make -j $N_PROCS
+
+# for new exe "python3.11"
+sudo make altinstall
+
+# for purdue servers. Let fail
+# load python with scipy >= 1.9.0
+ml python3/3.11
--- setup_vm.sh
+++ setup_vm.sh
@@ -6,17 +6,20 @@ sudo apt install -y build-essential vim
 
 # for all
 sudo apt install -y python3-pip python3-venv
-python3 -m venv venv_netsmith
-source venv_netsmith/bin/activate
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+
 
 # for dsent
 sudo apt install -y python-minimal python-dev
 sudo apt install -y cmake make
 # add cmake to path
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/bin/cmake:/usr/bin/cmake
+export PATH=$PATH:/usr/bin/cmake
 
 # for gem5
 sudo apt install -y scons zlib1g zlib1g-dev
 sudo apt install m4 libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev libboost-all-dev python-six libpng-dev swig
+
+
+python3 -m venv venv_netsmith
+source venv_netsmith/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
